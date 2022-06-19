c83b62211d97071bd44f801d38180b06f1cf02eb
luthfeew <luthfeew@gmail.com>
2022-06-19 20:58:27 +0700

    cleanup

--- dinduk/app/Http/Controllers/HomeController.php
+++ dinduk/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Identity;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data = Identity::all();
+        return view('home', compact('data'));
     }
 }
--- dinduk/resources/views/home.blade.php
+++ dinduk/resources/views/home.blade.php
@@ -3,17 +3,62 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+<h1>Data Identitas</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+<div class="row">
+    <div class="col">
+        <table class="table table-striped" id="myTable">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nomor ID</th>
+                    <th>Nama Lengkap</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Jenis ID</th>
+                    <th>Alamat</th>
+                    <th>Agama</th>
+                    <th>Status Perkawinan</th>
+                    <th>Tempat Lahir</th>
+                    <th>Tanggal Lahir</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($data as $row)
+                <tr>
+                    <td>{{$loop->iteration}}</td>
+                    <td>{{$row->idNumber}}</td>
+                    <td>{{$row->fullname}}</td>
+                    <td>{{$row->gender}}</td>
+                    <td>{{$row->IDType}}</td>
+                    <td>{{$row->address}}</td>
+                    <td>{{$row->religion}}</td>
+                    <td>{{$row->maritalStatus}}</td>
+                    <td>{{$row->pob}}</td>
+                    <td>{{$row->dob}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@stop
+
+@section('footer')
+    <!-- <h1>Footer</h1> -->
 @stop
 
 @section('css')
-    <!-- <link rel="stylesheet" href="/css/admin_custom.css"> -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css" />
 @stop
 
 @section('js')
-    <!-- <script> console.log('Hi!'); </script> -->
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#myTable').DataTable();
+    });
+</script>
 @stop
